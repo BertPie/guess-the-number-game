1212f3ea0c11aba0e53a341e3ec5d7f0c6666c6b
Bert Pie <BertPieGIT@gmail.com>
2019-01-21 12:12:25 +0100

    add properties

--- core/src/main/java/pie/bert/config/GameConfig.java
+++ core/src/main/java/pie/bert/config/GameConfig.java
@@ -1,14 +1,20 @@
 package pie.bert.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import pie.bert.GuessCount;
 import pie.bert.MaxNumber;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
-    private int maxNumber = 25;
-    private int guessCount = 8;
+
+    @Value("${game.maxNumber}")
+    private int maxNumber;
+    @Value("${game.guessCount}")
+    private int guessCount;
 
     @Bean
     @MaxNumber
