2a98feab7efa130edcb4fb828e5b692a493d2f02
Bert Pie <BertPieGIT@gmail.com>
2019-01-22 21:55:40 +0100

    create game service, add dev tools

---
+++ web/src/main/java/pie/bert/service/GameService.java
@@ -0,0 +1,9 @@
+package pie.bert.service;
+
+public interface GameService {
+    boolean isGameOver();
+    String getMainMessage();
+    String getResultMessage();
+    void checkGuess(int guess);
+    void reset();
+}
